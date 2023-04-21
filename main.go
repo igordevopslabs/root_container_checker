d3ae1df729b172add015dffc01b8c8518da2132c
igoritos22 <igoritos_22@hotmail.com>
2023-04-21 13:54:03 -0300

    Adding some metrics for prom

--- main.go
+++ main.go
@@ -3,44 +3,96 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
-func main() {
-	// rest.InClusterConfig() obtém a configuração do cluster a partir do ambiente em que o programa está sendo executado (dentro do cluster).
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err)
-	}
+var (
+	rootContainersCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "root_container_checker_total",
+			Help: "Total number of containers running as root.",
+		},
+		[]string{"pod", "namespace"},
+	)
+	podHealthGauge = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "root_container_checker_pod_health",
+			Help: "Health status of the root container checker Pod (1: healthy, 0: unhealthy).",
+		},
+	)
+)
 
-	// kubernetes.NewForConfig() cria um cliente para se comunicar com a API do Kubernetes usando a configuração obtida anteriormente.
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err)
-	}
+func init() {
+	prometheus.MustRegister(rootContainersCounter)
+	prometheus.MustRegister(podHealthGauge)
+}
 
-	// clientset.CoreV1().Pods("").List() lista todos os pods em todos os namespaces no cluster.
+func checkRootContainers(clientset *kubernetes.Clientset) {
+	// Set Pod health gauge to 1 (healthy) initially
+	podHealthGauge.Set(1)
+
 	pods, err := clientset.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error: %v\n", err)
+		// Set Pod health gauge to 0 (unhealthy) if there is an error
+		podHealthGauge.Set(0)
+		return
 	}
 
-	// Itera sobre todos os pods retornados.
 	for _, pod := range pods.Items {
 		rootContainers := 0
-
-		// Itera sobre todos os containers dentro de cada pod.
 		for _, container := range pod.Spec.Containers {
-			// Verifica se o container está executando como root.
 			if container.SecurityContext == nil || container.SecurityContext.RunAsUser == nil || *container.SecurityContext.RunAsUser == 0 {
 				rootContainers++
 			}
 		}
 
-		// Imprime o nome do pod, o namespace e a quantidade de containers executando como root.
-		fmt.Printf("Pod: %s, Namespace: %s, Root Containers: %d\n", pod.Name, pod.Namespace, rootContainers)
+		if rootContainers > 0 {
+			rootContainersCounter.WithLabelValues(pod.Name, pod.Namespace).Add(float64(rootContainers))
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/_ready", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "OK")
+	})
+
+	http.HandleFunc("/_healthy", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "OK")
+	})
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	go func() {
+		http.ListenAndServe(":8080", nil)
+	}()
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Configura o ticker para executar a verificação a cada 1h
+	ticker := time.NewTicker(3600 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		checkRootContainers(clientset)
+		<-ticker.C
 	}
 }
